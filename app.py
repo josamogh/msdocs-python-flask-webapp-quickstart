0ec40708cbd83c7237057e52dd1c26314c41a07d
josamogh <amoghjos273@gmail.com>
2023-05-23 15:47:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import (Flask, redirect, render_template, request,
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('https://sample273.azurewebsites.net/')
 def index():
    print('Request for index page received')
    return render_template('index.html')
